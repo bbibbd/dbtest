8154f786e7bb0d9736de96841af68bfcda9902d7
bbibbd <bbibochaa74@gmail.com>
2018-12-10 14:07:38 +0200

    student management program

---
+++ src/main/java/com/tistory/musit/student/StudentData.java
@@ -0,0 +1,42 @@
+package com.tistory.musit.student;
+
+public class StudentData {
+
+	private int id;
+	private String name;
+	private String gender;
+	private String major;
+	private String email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ src/main/java/com/tistory/musit/student/StudentDataManagement.java
@@ -0,0 +1,168 @@
+package com.tistory.musit.student;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDataManagement {
+    private Connection conn;
+    
+    private final static String URL = "jdbc:mysql://104.155.151.3/test";
+    private final static String ID = "root";
+    private final static String PW = "root";
+
+    public StudentDataManagement() {
+             
+        try {
+            conn = DriverManager.getConnection(URL,ID,PW);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+//insert
+    
+    public void insertStudent(StudentData student) {
+        String sql = "insert into STUDENTINFO values (?, ?, ?, ?, ?)";
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, student.getId());
+            pstmt.setString(2, student.getName());
+            pstmt.setString(3, student.getGender());
+            pstmt.setString(4, student.getMajor());
+            pstmt.setString(5, student.getEmail());
+             
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(pstmt != null && !pstmt.isClosed()) {
+                    pstmt.close();
+                }
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }    
+    }
+    
+//delete
+    public void deleteStudent(int studentID) {
+        String sql = "delete from STUDENTINFO where ID = ?";
+         
+        PreparedStatement pstmt = null;
+         
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, studentID);
+             
+            pstmt.executeUpdate();
+             
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(pstmt != null && !pstmt.isClosed()) {
+                    pstmt.close();
+                }
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }   
+    }
+    
+//update
+    public void updateStudent(StudentData student) {
+        String sql = "update STUDENTINFO set Name = ?, Gender = ? , Major = ?, Email = ? where ID = ?";
+        PreparedStatement pstmt = null;
+         
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, student.getName());
+            pstmt.setString(2, student.getGender());
+            pstmt.setString(3, student.getMajor());
+            pstmt.setString(4, student.getEmail());
+            pstmt.setInt(5, student.getId());
+             
+            pstmt.executeUpdate();
+             
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }       
+        finally {
+            try {
+                if(pstmt != null && !pstmt.isClosed()) {
+                    pstmt.close();
+                }
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+         
+         
+    }
+  
+    public StudentData selectOneStudent(int snum) {  
+        String sql = "select * from student where id = ?";
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        StudentData student = null;     
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, snum);
+             
+            rs = pstmt.executeQuery();
+             
+            while(rs.next()) {
+                student = new StudentData();
+                student.setId(rs.getInt("ID"));
+                student.setName(rs.getString("Name"));
+                student.setGender(rs.getString("Gender"));
+                student.setMajor(rs.getString("Major"));
+                student.setEmail(rs.getString("E-Mail"));
+            }       
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }       
+        return student;  
+    }
+        
+    public List<StudentData> selectAllStudent() {
+         
+        String sql = "select * from STUDENTINFO";
+         
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        StudentData student = null;
+        List<StudentData> studentList = new ArrayList<StudentData>();   
+        try {
+            pstmt = conn.prepareStatement(sql);
+             
+            rs = pstmt.executeQuery();
+             
+            while(rs.next()) {
+                student = new StudentData();
+                student.setId(rs.getInt("ID"));
+                student.setName(rs.getString("Name"));
+                student.setGender(rs.getString("Gender"));
+                student.setMajor(rs.getString("Major"));
+                student.setEmail(rs.getString("E-Mail"));
+                studentList.add(student);
+            }       
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }       
+        return studentList;
+    }
+
+}
